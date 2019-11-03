c040b59e83e09e85f233cd2c7c5c7ee55bf7c068
Julian <julian@minka.io>
2019-11-02 21:11:31 -0500

    feat(serviceSection) add service Section component

---
+++ src/components/organisms/serviceSection/serviceSection.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { H3, } from '../../atoms/h3Title';
+
+export default function ServiceCard() {
+  const text = 'Hola';
+  return (
+    <H3 text={text} />
+  );
+}
