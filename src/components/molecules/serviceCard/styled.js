2760ad24a7fef70edf9b4edb4d009e506a52e1bf
Julian <julian@minka.io>
2019-11-02 21:10:48 -0500

    feat(serviceCard) add service card component

---
+++ src/components/molecules/serviceCard/styled.js
@@ -0,0 +1,10 @@
+import Styled from 'styled-components';
+
+export const h3 = Styled.div`
+  color: ${props => props.theme.colors.primary};
+  font-size: 1.825em;
+  line-height: 1.4;
+  margin: 20px 0 0;
+  font-family: Montserrat,Helvetica,Arial,sans-serif;
+  font-weight: 300;
+`;
