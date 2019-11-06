3bdfbcf23058565a1bc89e1a98c9fbb35b6c51e5
Julian <julian@minka.io>
2019-11-05 21:20:05 -0500

    feat(serviceSection) adding style to service section

--- src/components/organisms/serviceSection/styled.js
+++ src/components/organisms/serviceSection/styled.js
@@ -1,10 +1,33 @@
 import Styled from 'styled-components';
 
-export const h3 = Styled.div`
-  color: ${props => props.theme.colors.primary};
-  font-size: 1.825em;
-  line-height: 1.4;
-  margin: 20px 0 0;
-  font-family: Montserrat,Helvetica,Arial,sans-serif;
-  font-weight: 300;
+export const Container = Styled.div`
+  background-color: rgba(245,245,245, 1.00);
+  padding-top: 6%;
+  padding-bottom: 3%;
+`;
+
+export const RowInnerWrapper = Styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+export const RowInner = Styled.div`
+  width: 1160px;
+  max-width: 100%;
+  margin: 0 auto;
+
+  @media screen and (min-width: 681px) {
+    display: flex;
+    flex-flow: wrap;
+  }
+
+  @media screen and(max-width: 760px) {
+    max-width: 90%;
+    font-size: .95em;
+  }
+
+  @media screen and(max-width: 1200px) {
+    max-width: 96%;
+    font-size: 1em;
+  }
 `;
